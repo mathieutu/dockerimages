01d48c5304371187c17142532004f5c0c21896ea
Mathieu TUDISCO <dev@mathieutu.ovh>
2017-10-23 15:58:06 +0200

    tweak xdebug for laravel-dev 7.1

--- laravel-dev_7.1/scripts/start.sh
+++ laravel-dev_7.1/scripts/start.sh
@@ -19,5 +19,14 @@ sed -i -e "s/worker_processes 5/worker_processes $procs/" /etc/nginx/nginx.conf
 # Set the right permissions (needed when mounting from a volume)
 chown -Rf www-data.www-data /usr/share/nginx/html/
 
+# Set the xdebug config if needed
+if [[ ! -z "$XDEBUG_ENABLE" ]] ; then
+    XdebugFile='/etc/php/7.1/fpm/conf.d/20-xdebug.ini'
+    echo "xdebug.remote_enable=1"  >> $XdebugFile
+
+    echo "xdebug.idekey=${XDEBUG_IDEKEY:-PHPSTORM}" >> $XdebugFile
+    echo "xdebug.remote_host=${XDEBUG_REMOTE_HOST:-docker.for.mac.localhost}" >> $XdebugFile
+fi
+
 # Start supervisord and services
 /usr/bin/supervisord -n -c /etc/supervisord.conf
